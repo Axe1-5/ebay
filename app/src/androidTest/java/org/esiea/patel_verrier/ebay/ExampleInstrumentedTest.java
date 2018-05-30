e56aac9f200a8140a06fd0c2bd47821947a92f76
safwaanep <s.patel@rt-iut.re>
2018-05-30 16:05:30 +0200

    Petites corrections

--- app/src/androidTest/java/org/esiea/patel_verrier/ebay/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/esiea/patel_verrier/ebay/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.akiya.myfirstapp;
+package org.esiea.patel_verrier.ebay;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.akiya.myfirstapp", appContext.getPackageName());
+        assertEquals("org.esiea.patel_verrier.ebay", appContext.getPackageName());
     }
 }
